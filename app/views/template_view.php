c2ae16c2953e47b1a202a511f8a451165d7e0a01
mardent <mar.dent@yandex.ru>
2015-11-23 16:20:38 +0400

    + Fix lang button view

--- app/views/template_view.php
+++ app/views/template_view.php
@@ -12,8 +12,8 @@ if(isset($_SESSION["lang"])){
 				<a href="/" alt="Домой"><img class="home_img" src="../images/home.png"></a>
 			<section class="header_button">
 				<section class="button_lang">
-					<button class="lang" id="english" onClick="Translator('en')"></button>
-					<button class="lang" id="russian" onClick="Translator('ru')"></button>
+					<button class="lang" id="english" onClick="Translator('en')"><img class="lang_img" src="../images/uk.png"></button>
+					<button class="lang" id="russian" onClick="Translator('ru')"><img class="lang_img" src="../images/rus.png"></button>
 				</section>
 <?php
 			if(@$_SESSION['user']){
