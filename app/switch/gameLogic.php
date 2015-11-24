d348ee8964358cb6aabbfcbb1f604ccc293b3e34
Bohdan Pedchenko <bogdan.innova@gmail.com>
2015-11-24 17:01:18 +0200

    Update gameLogic.php
    Исправил проблему с кодировкой русских символов

--- app/switch/gameLogic.php
+++ app/switch/gameLogic.php
@@ -1,14 +1,13 @@
-﻿<?php
+<?php
 class GameLogic {
 	function xmlActionExecution($action) {
 		$xml = simplexml_load_string("<a><a $action ></a></a>");
 		foreach($xml->a[0]->attributes() as $a => $b) {
 			switch($a) {
-				//èçìåíåíèå ïîëîæåíèÿ íà äîñêå
+				//location changing on the board
 				case "l" :
 					echo "LOCATION 1 = $b<br>";
 				break;				
-
 				case "l2" :
 					echo "LOCATION 2 = $b <br>";
 				break;
@@ -20,37 +19,37 @@ class GameLogic {
 				case "l4" :
 					echo "LOCATION 4 = $b <br>";
 				break;				
-				//äàâàòü ëè äåíüãè ïðè ïåðåñå÷åíèè ôèíèøà
+				//is money gives if "Go" is crossed
 				case "f" :
-					echo "íå äàâàòü äåíüãè ïðè ïåðåñå÷åíèè ôèíèøà <br>";
+					echo "don't give money if "Go" is crossed <br>";
 				break;	
-				//óâåëè÷èâàòü ëè ïëàòó äî ìàêñèìóìà ïðè ðåäèðåêòå íà íåêîòîðûå êîìïàíèè
+				//duplex pay for transport companies and 10x for water\electrycity
 				case "p" :
-					echo "óâåëè÷èâàòü ëè ïëàòó äî ìàêñèìóìà ïðè ðåäèðåêòå íà íåêîòîðûå êîìïàíèè <br>";
+					echo "duplex pay for transport companies and 10x for water\electrycity <br>";
 				break;					
-				//èçìåíåíèå áàëàíñà äåíåã
+				//changing money balance
 				case "m" :
-					echo "èçìåíåíèå áàëàíñà äåíåã = $b <br>";
+					echo "changing money balance on = $b <br>";
 				break;					
-				//ïëàòà äåíåã âñåì\îò âñåõ èãðîêîâ
+				//paying to\from all players
 				case "e" :
-					echo "ïëàòà äåíåã âñåì\îò âñåõ èãðîêîâ = $b <br>";
+					echo "paying to\from all players = $b <br>";
 				break;	
-				//ðåìîíò óëèö (çà äîì)
+				//homes repeiring
 				case "h" :
-					echo "ðåìîíò óëèö = $b <br>";
+					echo "homes repeiring = $b <br>";
 				break;	
-				//ðåìîíò óëèö (çà îòåëü)
+				//hotels repearing
 				case "o" :
-					echo "ðåìîíò óëèö = $b <br>";
+					echo "hotels repearing = $b <br>";
 				break;	
-				//âûõîä èç òþðüìû
+				//free jail lefting
 				case "n" :
-					echo "âûõîä èç òþðüìû <br>";
+					echo "free jail lefting <br>";
 				break;
-				//îòïðàâëÿéñÿ â òþðüìó
+				//got to jail
 				case "j" :
-					echo "îòïðàâëÿéñÿ â òþðüìó <br>";
+					echo "got to jail <br>";
 				break;
 			}
 		}
